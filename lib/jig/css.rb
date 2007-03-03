c20b402a2dd20fea88ad6078e7a1d243e8bb80d0
gwright <gwright@2566d0a7-165b-40c1-85e9-4272bc4bdc08>
2007-03-03 04:38:33 +0000

    Implemented >> for select list construction. Consolidated property list processing in #plist. Implemented property list merging.
    git-svn-id: svn://radar.local/jig/trunk@32 2566d0a7-165b-40c1-85e9-4272bc4bdc08

--- lib/jig/css.rb
+++ lib/jig/css.rb
@@ -3,15 +3,41 @@ require 'jig'
 
 class Jig
 	module CSS
+    module IntegerHelper
+      def in; "#{self}in"; end
+      def cm; "#{self}cm"; end
+      def mm; "#{self}mm"; end
+      def pt; "#{self}pt"; end
+      def pc; "#{self}pc"; end
+      def em; "#{self}em"; end
+      def ex; "#{self}ex"; end
+      def px; "#{self}px"; end
+      def pct; "#{self}%"; end
+    end
+
+    module FloatHelper
+      def pct; "%.2f%%" % (self*100); end
+    end
+
+    # When the CSS module is add the helper methods to core
+    def self.included(other)
+      Integer.send(:include, IntegerHelper)
+      Float.send(:include, FloatHelper)
+    end
+
 		# Element ID: 
 		def push_hash(hash)
 			push(*hash.map { |k,v| self.class.attribute(k, v) })
 		end
 		protected :push_hash
 
-    def plist(plist)
-      declarations = plist.map { |k,v| "#{k}: #{v}; " }.join
-      before(:__p, declarations)
+    def plist(plist=nil)
+      declarations = plist && plist.inject([]) { |d, (k,v)| d << "#{k}: #{v}; " }
+      if plist
+        before(:__p, *declarations) 
+      else
+        self
+      end
     end
 
     # Construct a selector with the current selector as the parent
@@ -19,31 +45,35 @@ class Jig
     # 
     #   (div > p).to_s     # 'div > p {}'
     def >(other)
-      before(:__s, ">", other.slice(0))
+      before(:__s, ">", other.slice(0)).before(:__p, other.slice(2))
     end
 
     def +(other)
-      before(:__s, "+", other.slice(0))
+      before(:__s, "+", other.slice(0)).before(:__p, other.slice(2))
     end
 
     def *(other)
-      before(:__s, "#", other.slice(0))
+      before(:__s, "#", other.slice(0)).before(:__p, other.slice(2))
     end
 
     def %(other)
-      before(:__s, ":", other.slice(0))
+      before(:__s, ":", other.slice(0)).before(:__p, other.slice(2))
     end
-
-    #def -@
-    #  self.class.new("#", self)
-    #end
 
     def >>(other)
-      before(:__s, " ", other.slice(0))
+      before(:__s, " ", other.slice(0)).before(:__p, other.slice(2))
     end
 
-    def method_missing(sym)
-      before(:__s, ".#{sym}")
+    def <<(other)
+      before(:__s, ", ", other.slice(0)).before(:__p, other.slice(2))
+    end
+
+    def ^(plist)
+      plist(plist)
+    end
+
+    def method_missing(sym, plist=nil)
+      before(:__s, ".#{sym}").plist(plist)
     end
 
     def [](*args)
@@ -75,12 +105,9 @@ class Jig
 		Encode = Hash[*%w{& amp " quot > gt < lt}]
 
     def rule(selector="", plist=nil)
-      #base = (@_rule ||= new(:__s, " {\n  ", :__ps, :__p, "}\n").freeze)
       base = (@_rule ||= new(:__s, " {", :__ps, :__p, "}").freeze)
-      declarations = plist && plist.map { |k,v| "#{k}: #{v}; " }.join
       base = base.before(:__s, selector) if selector
-      base = base.before(:__p, declarations) if declarations
-      base
+      base.plist(plist)
     end
 
     # Generate a universal selector rule
